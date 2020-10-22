69a2f7e7084f3c333220d076faee28ee1932eb9a
Sylvester Musyoki <sylvester.musyoki@cellulant.com>
2020-10-22 13:03:06 +0300

    Add History

--- app/src/main/java/com/skills/rentaride/ui/adapter/LendHistoryViewHolder.kt
+++ app/src/main/java/com/skills/rentaride/ui/adapter/LendHistoryViewHolder.kt
@@ -1,9 +1,11 @@
 package com.skills.rentaride.ui.adapter
 
+import android.annotation.SuppressLint
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.skills.rentaride.model.LendTransactionDTO
 import com.skills.rentaride.utils.getProgressDrawable
+import com.skills.rentaride.utils.loadImage
 import kotlinx.android.synthetic.main.item_history.view.*
 
 class LendHistoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -12,18 +14,22 @@ class LendHistoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val itemType = view.itemType
 
 //    // Get the image view.
-//    private val imageView = view.imageView
+    private val imageView = view.imageView
 
-    // Get the country capital.
     private val serialNumber = view.serialNumber
+    private val itemCost = view.lendItemCost
+    private val itemStatus = view.lendItemState
 
     // Get the progress drawable.
     private val progressDrawable = getProgressDrawable(view.context)
 
 
+    @SuppressLint("SetTextI18n")
     fun bindItems(lendTransactionDTO: LendTransactionDTO) {
         itemType.text = lendTransactionDTO.lendItemTypeName
         serialNumber.text = lendTransactionDTO.serialNumber
-//        imageView.loadImage(country.flag,progressDrawable)
+        itemCost.text = "KES: "+lendTransactionDTO.lendItemCost.toString()+"0"
+        itemStatus.text = lendTransactionDTO.lendTransactionStatus
+        imageView.loadImage("https://www.flaticon.com/svg/static/icons/svg/3596/3596080.svg", progressDrawable)
     }
 }
